83dba94b38d94e4171d9a4627595b763be68bc0c
Pisotski <vpisotski@gmail.com>
2018-05-08 20:05:17 -0700

    added enter

--- src/views/search.js
+++ src/views/search.js
@@ -1,9 +1,16 @@
 var SearchView = Backbone.View.extend({
 
   events:{
-    'click button': 'handleClick'
+    'click button': 'handleClick',
+    'keypress': 'checkKeycode'
   },
   
+  checkKeycode : function(e){
+    var code = e.keyCode;
+    if (code === 13) {
+      this.handleClick();
+    }
+  },
   handleClick: function(){
     this.collection.search($('.form-control').val());
     $('.form-control').val('');
